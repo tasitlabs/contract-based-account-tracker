bcaa284a9bbc273f20ab2b4133ed4070d018773f
Paul Cowgill <pauldavidcowgill@gmail.com>
2019-05-13 14:07:01 -0500

    Fixed the env link and customized more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react'
-import ApolloClient, { gql, InMemoryCache } from 'apollo-boost'
-import { ApolloProvider, Query } from 'react-apollo'
+import React, { Component } from "react";
+import ApolloClient, { gql, InMemoryCache } from "apollo-boost";
+import { ApolloProvider, Query } from "react-apollo";
 import {
   Grid,
   LinearProgress,
@@ -9,55 +9,65 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Button,
-} from '@material-ui/core'
-import './App.css'
-import Header from './components/Header'
-import Error from './components/Error'
-import Gravatars from './components/Gravatars'
-import Filter from './components/Filter'
+  Button
+} from "@material-ui/core";
+import "./App.css";
+import Header from "./components/Header";
+import Error from "./components/Error";
+import Gravatars from "./components/Gravatars";
+import Filter from "./components/Filter";
 
 if (!process.env.REACT_APP_GRAPHQL_ENDPOINT) {
-  throw new Error('REACT_APP_GRAPHQL_ENDPOINT environment variable not defined')
+  throw new Error(
+    "REACT_APP_GRAPHQL_ENDPOINT environment variable not defined"
+  );
 }
 
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL_ENDPOINT,
-  cache: new InMemoryCache(),
-})
+  cache: new InMemoryCache()
+});
 
 const GRAVATARS_QUERY = gql`
   query gravatars($where: Gravatar_filter!, $orderBy: Gravatar_orderBy!) {
-    gravatars(first: 100, where: $where, orderBy: $orderBy, orderDirection: asc) {
+    gravatars(
+      first: 100
+      where: $where
+      orderBy: $orderBy
+      orderDirection: asc
+    ) {
       id
       owner
       displayName
       imageUrl
     }
   }
-`
+`;
 
 class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       withImage: false,
       withName: false,
-      orderBy: 'displayName',
-      showHelpDialog: false,
-    }
+      orderBy: "displayName",
+      showHelpDialog: false
+    };
   }
 
   toggleHelpDialog = () => {
-    this.setState(state => ({ ...state, showHelpDialog: !state.showHelpDialog }))
-  }
+    this.setState(state => ({
+      ...state,
+      showHelpDialog: !state.showHelpDialog
+    }));
+  };
 
   gotoQuickStartGuide = () => {
-    window.location.href = 'https://thegraph.com/docs/quick-start'
-  }
+    window.location.href = "https://tasit.io";
+  };
 
   render() {
-    const { withImage, withName, orderBy, showHelpDialog } = this.state
+    const { withImage, withName, orderBy, showHelpDialog } = this.state;
 
     return (
       <ApolloProvider client={client}>
@@ -68,12 +78,20 @@ class App extends Component {
               orderBy={orderBy}
               withImage={withImage}
               withName={withName}
-              onOrderBy={field => this.setState(state => ({ ...state, orderBy: field }))}
+              onOrderBy={field =>
+                this.setState(state => ({ ...state, orderBy: field }))
+              }
               onToggleWithImage={() =>
-                this.setState(state => ({ ...state, withImage: !state.withImage }))
+                this.setState(state => ({
+                  ...state,
+                  withImage: !state.withImage
+                }))
               }
               onToggleWithName={() =>
-                this.setState(state => ({ ...state, withName: !state.withName }))
+                this.setState(state => ({
+                  ...state,
+                  withName: !state.withName
+                }))
               }
             />
             <Grid item>
@@ -82,20 +100,23 @@ class App extends Component {
                   query={GRAVATARS_QUERY}
                   variables={{
                     where: {
-                      ...(withImage ? { imageUrl_starts_with: 'http' } : {}),
-                      ...(withName ? { displayName_not: '' } : {}),
+                      ...(withImage ? { imageUrl_starts_with: "http" } : {}),
+                      ...(withName ? { displayName_not: "" } : {})
                     },
-                    orderBy: orderBy,
+                    orderBy: orderBy
                   }}
                 >
                   {({ data, error, loading }) => {
                     return loading ? (
-                      <LinearProgress variant="query" style={{ width: '100%' }} />
+                      <LinearProgress
+                        variant="query"
+                        style={{ width: "100%" }}
+                      />
                     ) : error ? (
                       <Error error={error} />
                     ) : (
                       <Gravatars gravatars={data.gravatars} />
-                    )
+                    );
                   }}
                 </Query>
               </Grid>
@@ -107,26 +128,30 @@ class App extends Component {
             onClose={this.toggleHelpDialog}
             aria-labelledby="help-dialog"
           >
-            <DialogTitle id="help-dialog">{'Show Quick Guide?'}</DialogTitle>
+            <DialogTitle id="help-dialog">{"What's Tasit?"}</DialogTitle>
             <DialogContent>
               <DialogContentText>
-                We have prepared a quick guide for you to get started with The Graph at
-                this hackathon. Shall we take you there now?
+                There's more info about the Tasit project over at tasit.io.
+                Should we take you there now?
               </DialogContentText>
             </DialogContent>
             <DialogActions>
               <Button onClick={this.toggleHelpDialog} color="primary">
                 Nah, I'm good
               </Button>
-              <Button onClick={this.gotoQuickStartGuide} color="primary" autoFocus>
-                Yes, pease
+              <Button
+                onClick={this.gotoQuickStartGuide}
+                color="primary"
+                autoFocus
+              >
+                Yes, please
               </Button>
             </DialogActions>
           </Dialog>
         </div>
       </ApolloProvider>
-    )
+    );
   }
 }
 
-export default App
+export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { Grid, Typography, IconButton } from '@material-ui/core'
-import HelpIcon from '@material-ui/icons/Help'
+import React from "react";
+import { Grid, Typography, IconButton } from "@material-ui/core";
+import HelpIcon from "@material-ui/icons/Help";
 
 const Header = ({ onHelp }) => (
   <Grid container direction="row" alignItems="center" spacing={16}>
     <Grid item>
-      <Typography variant="title">The Graph ETHDenver dApp</Typography>
+      <Typography variant="title">
+        Tasit The Graph Hackathon Web Dapp
+      </Typography>
     </Grid>
     <Grid item>
       <IconButton
@@ -17,6 +19,6 @@ const Header = ({ onHelp }) => (
       </IconButton>
     </Grid>
   </Grid>
-)
+);
 
-export default Header
+export default Header;
